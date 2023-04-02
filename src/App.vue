8ffe7d0271b8f2510f0b7706c7c48fa2ee3497e7
javiBarrientos <javibarrientosfernandez99@gmail.com>
2023-04-02 13:33:03 +0200

    import components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <v-app>
-    <h1>Hello world</h1>
+    <div class="row d-flex">
+      <div class="col-3"><sidebar-content-vue /></div>
+      <div class="col-9"><main-content-vue /></div>
+    </div>
   </v-app>
 </template>
 
 <script>
-// import { ProductService } from "./service/ProductsService";
+import MainContentVue from "./components/Main/MainContent.vue";
+import SidebarContentVue from "./components/Sidebar/SidebarContent.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { SidebarContentVue, MainContentVue },
   data: () => ({}),
 };
 </script>
