d09b57872f2033b89253f73593dd83671eb3cd02
javiBarrientos <javibarrientosfernandez99@gmail.com>
2023-04-02 15:13:41 +0200

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
-    <div class="row d-flex">
-      <div class="col-3"><sidebar-content-vue /></div>
-      <div class="col-9"><main-content-vue /></div>
-    </div>
+    <v-row class="d-flex">
+      <v-col cols="5" sm="5" md="3" lg="2"><sidebar-content-vue /></v-col>
+      <v-col cols="7" sm="7" md="9" lg="10"><main-products-vue /></v-col>
+    </v-row>
   </v-app>
 </template>
 
 <script>
-import MainContentVue from "./components/Main/MainContent.vue";
 import SidebarContentVue from "./components/Sidebar/SidebarContent.vue";
+import MainProductsVue from "./components/Main/MainProducts.vue";
 
 export default {
   name: "App",
-  components: { SidebarContentVue, MainContentVue },
+  components: { SidebarContentVue, MainProductsVue },
   data: () => ({}),
 };
 </script>
